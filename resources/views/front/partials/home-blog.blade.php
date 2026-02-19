0a7a8a60055ab52b3dd77cf8522c82057c61b648
Sujon Office <>
2026-02-19 16:00:54 +0600

    home page - done

---
+++ resources/views/front/partials/home-blog.blade.php
@@ -0,0 +1,78 @@
+
+<section class="ulockd-blog">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2 text-center">
+                <div class="ulockd-main-title">
+                    <h2 class="text-uppercase">LATEST <span class="text-thm2">NEWS</span></h2>
+                    <h4>Your Attention Is Changed The Part Of World.Give a helping hand to those who need it!</h4>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-6 col-md-4">
+                <article class="ulockd-blog-post ulockd-mrgn630">
+                    <div class="post-thumb">
+                        <div class="img-post-icon ulockd-bgthm"><span class="fa fa-image"></span></div>
+                        <img class="img-responsive img-whp" src="{{ asset('front/images/blog/1.jpg') }}" alt="1.jpg') }}">
+                        <ul class="list-inline posted-date">
+                            <li><a class="text-thm2" href="#"><span class="flaticon-people-2"></span> Diade3007</a></li>
+                            <li><a class="text-thm2" href="#"><span class="flaticon-chat text-thm2"></span> 9 Comment</a></li>
+                        </ul>
+                    </div>
+                    <div class="bp-details one text-left">
+                        <div class="bp-date pull-left">
+                            <span class="day">26</span> <br>
+                            <span class="time">Sep 17</span>
+                        </div>
+                        <h3 class="post-title">Our Latest Post</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus consequatur hic, harum aliquid aperiam fuga beatae laudantium velit, laborum minus omnis itaque nostrum, nisi quod!</p>
+                        <button type="submit" class="btn btn-default ulockd-btn-thm2">Read More</button>
+                    </div>
+                </article>
+            </div>
+            <div class="col-sm-6 col-md-4">
+                <article class="ulockd-blog-post">
+                    <div class="bp-details one text-left">
+                        <div class="bp-date pull-left">
+                            <span class="day">26</span> <br>
+                            <span class="time">Sep 17</span>
+                        </div>
+                        <h3 class="post-title">Our Latest Post</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus consequatur hic, harum aliquid aperiam fuga beatae laudantium velit, laborum minus omnis itaque nostrum, nisi quod!</p>
+                        <button type="submit" class="btn btn-default ulockd-btn-thm2">Read More</button>
+                    </div>
+                    <div class="post-thumb">
+                        <div class="img-post-icon ulockd-bgthm"><span class="fa fa-image"></span></div>
+                        <img class="img-responsive img-whp" src="{{ asset('front/images/blog/2.jpg') }}" alt="2.jpg') }}">
+                        <ul class="list-inline posted-date">
+                            <li><a class="text-thm2" href="#"><span class="flaticon-people-2"></span> Diade3007</a></li>
+                            <li><a class="text-thm2" href="#"><span class="flaticon-chat text-thm2"></span> 9 Comment</a></li>
+                        </ul>
+                    </div>
+                </article>
+            </div>
+            <div class="col-sm-6 col-md-4">
+                <article class="ulockd-blog-post ulockd-mrgn630">
+                    <div class="post-thumb">
+                        <div class="img-post-icon ulockd-bgthm"><span class="fa fa-image"></span></div>
+                        <img class="img-responsive img-whp" src="{{ asset('front/images/blog/3.jpg') }}" alt="3.jpg') }}">
+                        <ul class="list-inline posted-date">
+                            <li><a class="text-thm2" href="#"><span class="flaticon-people-2"></span> Diade3007</a></li>
+                            <li><a class="text-thm2" href="#"><span class="flaticon-chat text-thm2"></span> 9 Comment</a></li>
+                        </ul>
+                    </div>
+                    <div class="bp-details one text-left">
+                        <div class="bp-date pull-left">
+                            <span class="day">26</span> <br>
+                            <span class="time">Sep 17</span>
+                        </div>
+                        <h3 class="post-title">Our Latest Post</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus consequatur hic, harum aliquid aperiam fuga beatae laudantium velit, laborum minus omnis itaque nostrum, nisi quod!</p>
+                        <button type="submit" class="btn btn-default ulockd-btn-thm2">Read More</button>
+                    </div>
+                </article>
+            </div>
+        </div>
+    </div>
+</section>
---
+++ resources/views/front/partials/home-testimonial.blade.php
@@ -0,0 +1,44 @@
+<section class="ulockd-testimonial">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2 text-center">
+                <div class="ulockd-main-title">
+                    <h2 class="text-uppercase">Our <span class="text-thm2">Testimonials</span></h2>
+                    <h4>Your Attention Is Changed The Part Of World.Give a helping hand to those who need it!</h4>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-6 col-md-6 col-lg-4">
+                <div class="ulockd-testimonials">
+                    <div class="testi-thumb pull-left"><img class="img-responsive img-whp thumbnail" src="{{ asset('front/images/testimonial/1.jpg') }}" alt="1.jpg') }}"></div>
+                    <div class="testi-details">
+                        <h4>Daniel Forg</h4>
+                        <p class="text-thm2">-Oxford Professor</p>
+                        <p>Consectetur adipisicing elit. Maiores a molestias, exercitationem at? Neque eaque modi minus consequuntur sapiente quasi amet? Animi.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-6 col-lg-4">
+                <div class="ulockd-testimonials">
+                    <div class="testi-thumb pull-left"><img class="img-responsive img-whp thumbnail" src="{{ asset('front/images/testimonial/2.jpg') }}" alt="2.jpg') }}"></div>
+                    <div class="testi-details">
+                        <h4>Daniel Forg</h4>
+                        <p class="text-thm2">-Oxford Professor</p>
+                        <p>Consectetur adipisicing elit. Maiores a molestias, exercitationem at? Neque eaque modi minus consequuntur sapiente quasi amet? Animi.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-6 col-lg-4">
+                <div class="ulockd-testimonials">
+                    <div class="testi-thumb pull-left"><img class="img-responsive img-whp thumbnail" src="{{ asset('front/images/testimonial/3.jpg') }}" alt="3.jpg') }}"></div>
+                    <div class="testi-details">
+                        <h4>Daniel Forg</h4>
+                        <p class="text-thm2">-Oxford Professor</p>
+                        <p>Consectetur adipisicing elit. Maiores a molestias, exercitationem at? Neque eaque modi minus consequuntur sapiente quasi amet? Animi.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
